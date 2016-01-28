d8d6504390bf2234816a5c10bb40df88ea2ea9ea
TOM WOR <mail@tomwor.com>
2016-01-28 13:35:53 +0100

    Added powerup and enemy spawn based on stresslevel

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -4,110 +4,104 @@ using System;
 
 public class GameController : SceneController
 {
-    public Color[] stressLevelColors;
+	public Color[] stressLevelColors;
 
-    private static int currentStressLevel = 0;
-    public static int CurrentStressLevel
-    {
-        get { return GameController.currentStressLevel; }
-        set
-        {
-            GameController.currentStressLevel = value;
-        }
-    }
+	private static int currentStressLevel = 0;
 
-    private static TrackRider player;
-    public static TrackRider Player
-    {
-        get { return player; }
-        set
-        {
-            GameController.player = value;
-            EventManager.PlayerSpawned(value);
-        }
-    }
+	public static int CurrentStressLevel {
+		get { return GameController.currentStressLevel; }
+		set {
+			GameController.currentStressLevel = value;
+		}
+	}
+
+	private static TrackRider player;
+
+	public static TrackRider Player {
+		get { return player; }
+		set {
+			GameController.player = value;
+			EventManager.PlayerSpawned(value);
+		}
+	}
 
 
-    public void Start()
-    {
+	public void Start()
+	{
 		Application.targetFrameRate = 60;
-        GameController.currentStressLevel = 0;
+		GameController.currentStressLevel = 0;
 
-        SRDebug.Instance.PanelVisibilityChanged += visible =>
-        {
-            if (visible)
-            {
-              GameController.PauseGame();
-            } else {
-              GameController.UnPauseGame();
-            }
-        };
+		SRDebug.Instance.PanelVisibilityChanged += visible => {
+			if (visible) {
+				GameController.PauseGame();
+			} else {
+				GameController.UnPauseGame();
+			}
+		};
 
 
-        GameObject TrackGeneratorObject = Instantiate(Resources.Load("TrackGenerator", typeof(GameObject))) as GameObject;
-        GameController.TrackGenerator = TrackGeneratorObject.GetComponent<TrackGenerator>();
+		GameObject TrackGeneratorObject = Instantiate(Resources.Load("TrackGenerator", typeof(GameObject))) as GameObject;
+		GameController.TrackGenerator = TrackGeneratorObject.GetComponent<TrackGenerator>();
 
-        GameObject PlayerObject = Instantiate(Resources.Load("Player", typeof(GameObject))) as GameObject;
-        GameController.Player = PlayerObject.GetComponent<Player>();
-        GameController.Player.TrackGenerator = GameController.TrackGenerator;
+		GameObject PlayerObject = Instantiate(Resources.Load("Player", typeof(GameObject))) as GameObject;
+		GameController.Player = PlayerObject.GetComponent<Player>();
+		GameController.Player.TrackGenerator = GameController.TrackGenerator;
 
-        EventManager.PlayerSpawned(GameController.Player);
+		EventManager.PlayerSpawned(GameController.Player);
 
 		this.setInitialStressLevel();
-        StartCoroutine(this.UpdateStressLevel());
-    }
+		StartCoroutine(this.UpdateStressLevel());
+	}
 
 
-    public void OnLevelWasLoaded()
-    {
+	public void OnLevelWasLoaded()
+	{
 		this.setInitialStressLevel();
-    }
+	}
 
 
 	private void setInitialStressLevel()
 	{
-        GameController.currentStressLevel = 0;
-        SceneController.currentTrackBorderColor = this.stressLevelColors[GameController.currentStressLevel];
+		GameController.currentStressLevel = 0;
+		SceneController.currentTrackBorderColor = this.stressLevelColors[GameController.currentStressLevel];
 	}
 
 
-    public IEnumerator UpdateStressLevel()
-    {
-        while(true)
-        {
-            yield return new WaitForSeconds(15.0f);
+	public IEnumerator UpdateStressLevel()
+	{
+		while (true) {
+			yield return new WaitForSeconds(20.0f);
 
-            GameController.currentStressLevel++;
-            //Debug.Log("Entering Level: "+GameController.currentStressLevel);
+			GameController.currentStressLevel++;
+			Debug.Log("Entering Level: " + GameController.currentStressLevel);
 
-            if( this.stressLevelColors.Length > GameController.currentStressLevel )
-            {
-                SceneController.currentTrackBorderColor = this.stressLevelColors[GameController.currentStressLevel];
-                EventManager.TrackBorderColorChanged(SceneController.currentTrackBorderColor);
-            }
-        }
+			if (this.stressLevelColors.Length > GameController.currentStressLevel) {
+				SceneController.currentTrackBorderColor = this.stressLevelColors[GameController.currentStressLevel];
+				EventManager.TrackBorderColorChanged(SceneController.currentTrackBorderColor);
+			}
+		}
 
-    }
+	}
 
 
-    // Pause the game
-    public static void PauseGame()
-    {
-        Time.timeScale = 0.0f;
-    }
+	// Pause the game
+	public static void PauseGame()
+	{
+		Time.timeScale = 0.0f;
+	}
 
 
-    // Unpause the game
-    public static void UnPauseGame()
-    {
-        Time.timeScale = 1.0f;
-    }
+	// Unpause the game
+	public static void UnPauseGame()
+	{
+		Time.timeScale = 1.0f;
+	}
 
 
-    // Check if game is paused
-    public static bool isPaused()
-    {
-        return Time.timeScale == 0.0f;
-    }
+	// Check if game is paused
+	public static bool isPaused()
+	{
+		return (int)Time.timeScale == (int)0.0f;
+	}
 
 }
--- Assets/Scripts/Track/Spawn.cs
+++ Assets/Scripts/Track/Spawn.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 public class Spawn : MonoBehaviour
 {
 	public int likelyhood = 5;
+	public int stressLevel = 0;
 	public int spawnType = 1;
 }
